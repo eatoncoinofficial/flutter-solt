cec56732890645574a4dd0f8e21a4206161142a0
teste <teste@teste.com>
2024-12-25 18:25:34 +0800

    11

---
+++ lib/app.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:triple_seven_slots_game/bloc/slot_machine_bloc/slot_machine_bloc.dart';
+import 'package:triple_seven_slots_game/bloc/spin_wheel_cubit/spin_wheel_cubit.dart';
+import 'package:triple_seven_slots_game/bloc/user_balance_cubit/user_balance_cubit.dart';
+import 'package:triple_seven_slots_game/database/user_data_local_storage.dart';
+import 'package:triple_seven_slots_game/repositories/user_data_repository.dart';
+import 'package:triple_seven_slots_game/routes.dart';
+
+class App extends StatelessWidget {
+  final UserDataRepository userDataRepository =
+      UserDataRepositoryImpl(userBalanceLocalStorage: UserDataLocalStorageImpl());
+  App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SlotMachineBloc>(
+          create: (context) => SlotMachineBloc(),
+        ),
+        BlocProvider<SpinWheelCubit>(
+            create: (context) => SpinWheelCubit(userDataRepository: userDataRepository)),
+        BlocProvider<UserBalanceCubit>(
+            create: (context) => UserBalanceCubit(userBalanceRepository: userDataRepository)),
+      ],
+      child: MaterialApp(
+        title: 'Triple Seven Slots Game',
+        // builder: (context, child) =>
+        //     ResponsiveWrapper.builder(child, defaultScale: true, breakpoints: [
+        //   const ResponsiveBreakpoint.autoScale(500, name: MOBILE),
+        //   const ResponsiveBreakpoint.autoScale(600, name: TABLET),
+        //   const ResponsiveBreakpoint.autoScale(800, name: DESKTOP),
+        // ]),
+        builder: (context, child) => ResponsiveBreakpoints.builder(
+          breakpoints: [
+            const Breakpoint(start: 0, end: 450, name: MOBILE),
+            const Breakpoint(start: 451, end: 800, name: TABLET),
+            const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+            const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+          ],
+          child: child!,
+        ),
+        routes: routes,
+        initialRoute: splashScreenRoute,
+      ),
+    );
+  }
+}
---
+++ lib/utils/precaching.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
+import 'package:triple_seven_slots_game/assets.dart';
+
+class Precaching {
+  Future<bool> loadGraphics(BuildContext context) async {
+    await _precacheImages(context);
+    await _loadComposition();
+    // await svg.cache.putIfAbsent(SvgAssetLoader(boardIc).cacheKey(null), () => SvgAssetLoader(boardIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(playButton).cacheKey(null), () => SvgAssetLoader(playButton).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(arrowLeftIc).cacheKey(null), () => SvgAssetLoader(arrowLeftIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(arrowRightIc).cacheKey(null), () => SvgAssetLoader(arrowRightIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(seventhIc).cacheKey(null), () => SvgAssetLoader(seventhIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(appleIc).cacheKey(null), () => SvgAssetLoader(appleIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(barIc).cacheKey(null), () => SvgAssetLoader(barIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(lemonIc).cacheKey(null), () => SvgAssetLoader(lemonIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(coinIc).cacheKey(null), () => SvgAssetLoader(coinIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(watermelonIc).cacheKey(null), () => SvgAssetLoader(watermelonIc).loadBytes(null));
+    // await svg.cache.putIfAbsent(SvgAssetLoader(crownIc).cacheKey(null), () => SvgAssetLoader(crownIc).loadBytes(null));
+
+    return true;
+  }
+
+  Future<void> _precacheImages(BuildContext context) async {
+    await Future.wait([
+      precacheImage(Image.asset(wheelBorder).image, context),
+      precacheImage(Image.asset(spinningPointer).image, context),
+      precacheImage(Image.asset(star).image, context),
+      precacheImage(Image.asset(wheelTitleIc).image, context),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, boardIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, playButton), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, arrowLeftIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, arrowRightIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, seventhIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, appleIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, barIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, lemonIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, coinIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, watermelonIc), null),
+      // precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, crownIc), null),
+    ]);
+  }
+
+  Future<void> _loadComposition() async {
+    await Future.wait([
+      AssetLottie(confettiLottie).load(),
+      AssetLottie(goldenConfettiLottie).load(),
+      AssetLottie(coinsLottie).load(),
+    ]);
+  }
+}
